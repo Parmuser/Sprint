085d0bbeaad04659c0667b5223ea730ba2653a15
Parm28 <Yparmuser@gmail.com>
2025-12-08 14:50:36 +0530

    latest changes

--- User/src/main/java/com/example/user/DemoApplication.java
+++ User/src/main/java/com/example/user/DemoApplication.java
@@ -2,14 +2,11 @@ package com.example.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import java.util.TimeZone;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		// Set the default timezone to avoid Asia/Calcutta issues
-		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"));
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
